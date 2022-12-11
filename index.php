40a4e28631d783a6105317486e3a829147a01cff
yanda94 <yanda@jaringpangan.com>
2022-12-11 11:40:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/infa/logo-medium.png">
+  <link rel="icon" type="image/png" href="./assets/img/infa/logo-medium.png">
+  <title>
+    InfaCoin | Token
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Nucleo Icons -->
+  <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="./assets/css/blk-design-system.css?v=1.0.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="./assets/infa/infa.css" rel="stylesheet" />
+  <!-- FontAwesome -->
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+</head>
+
+<body class="index-page">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg fixed-top navbar-transparent ">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://coin.infranesia.com" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+          <img src="./assets/img/infa/logo-small.png" width="15%"><span> Infa</span>Coin
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <div class="navbar-collapse-header">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a>
+                InfaCoin
+              </a>
+            </div>
+            <div class="col-6 collapse-close text-right">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="tim-icons icon-simple-remove"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <ul class="navbar-nav">
+          <li class="nav-item p-0">
+            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/halo_infranesia" target="_blank">
+              <i class="fab fa-twitter"></i>
+              <p class="d-lg-none d-xl-none">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item p-0">
+            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/infranesia_official" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+              <p class="d-lg-none d-xl-none">Facebook</p>
+            </a>
+          </li>
+          <li class="nav-item p-0">
+            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/infranesia_official" target="_blank">
+              <i class="fab fa-instagram"></i>
+              <p class="d-lg-none d-xl-none">Instagram</p>
+            </a>
+          </li>
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="fa fa-cogs d-lg-none d-xl-none"></i> Getting started
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="#" class="dropdown-item">
+                <i class="tim-icons icon-paper"></i> Documentation
+              </a>
+              <a href="https://infranesia.com" class="dropdown-item">
+                <i class="tim-icons icon-image-02"></i>Infranesia.com
+              </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-default d-none d-lg-block" href="https://raydium.io/swap?inputCurrency=sol&outputCurrency=EpKFYk3mCtoWT11itNswZgdGy7SQyvRhRJKpkFetxixj&fixed=in" target="_blank" onclick="scrollToDownload()">
+              <i class="fas fa-coins"></i> Buy Coin INFA
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="wrapper">
+    <div class="page-header header-filter">
+      <div class="squares square1"></div>
+      <div class="squares square2"></div>
+      <div class="squares square3"></div>
+      <div class="squares square4"></div>
+      <div class="squares square5"></div>
+      <div class="squares square6"></div>
+      <div class="squares square7"></div>
+      <div class="container">
+        <div class="content-center brand">
+          <h1 class="h1-seo">The Infa Token</h1>
+          <h3>Explore the utility that serves the InfaCoin ecosystem.</h3>
+          <a href="https://raydium.io/swap?inputCurrency=sol&outputCurrency=EpKFYk3mCtoWT11itNswZgdGy7SQyvRhRJKpkFetxixj&fixed=in" class="btn btn-success" target="_blank">Raydium</a>
+          <a href="https://solscan.io/token/EpKFYk3mCtoWT11itNswZgdGy7SQyvRhRJKpkFetxixj#holders" class="btn btn-info" target="_blank">Solscan</a>
+        </div>
+      </div>
+    </div>
+
+    <section class="section section-lg bg-info" id="purpose">
+      <section class="section">
+        <img src="./assets/img/path4.png" class="path">
+        <div class="container">
+          <div class="row row-grid justify-content-between">
+            <div class="col-md-6">
+              <div class="pl-md-5">
+                <h1>Purpose
+                  <br />
+                </h1>
+                <p>I should be capable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. </p>
+                <br />
+                <p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray.</p>
+                <br />
+              </div>
+            </div>
+            <div class="col-md-5 mt-lg-5">
+              <div class="row">
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats ">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-trophy text-warning"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">3,237
+                            <p>
+                            <p class="card-category">Awards</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats upper bg-default">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-coins text-white"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">3,653
+                            <p>
+                            <p class="card-category">Commits</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats ">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-gift-2 text-info"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">593
+                            <p>
+                            <p class="card-category">Presents</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats ">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-credit-card text-success"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">10,783
+                            <p>
+                            <p class="card-category">Forks</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+
+    <section class="section section-lg" id="explore">
+      <section class="section">
+        <div class="section section-nucleo-icons">
+          <img src="assets/img/path3.png" class="path">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-8 col-md-12">
+                <h2 class="title">Explore InfaCoin</h2>
+                <h4 class="description">
+                  InfaCoin is available on Solscan and Raydium.
+                </h4>
+                <div class="btn-wrapper">
+                  <a href="https://solscan.io/token/EpKFYk3mCtoWT11itNswZgdGy7SQyvRhRJKpkFetxixj#holders" class="btn btn-primary btn-simple btn-round btn-lg" target="_blank">View</a>
+                </div>
+              </div>
+            </div>
+            <div class="blur-hover">
+              <a href="https://solscan.io/token/EpKFYk3mCtoWT11itNswZgdGy7SQyvRhRJKpkFetxixj#holders">
+                <div class="icons-container blur-item on-screen mt-5">
+                  <!-- Center -->
+                  <i class="icon tim-icons icon-coins"></i>
+                  <!-- Right 1 -->
+                  <i class="icon icon-sm tim-icons icon-spaceship"></i>
+                  <i class="icon icon-sm tim-icons icon-money-coins"></i>
+                  <i class="icon icon-sm tim-icons icon-link-72"></i>
+                  <!-- Right 2 -->
+                  <i class="icon tim-icons icon-send"></i>
+                  <i class="icon tim-icons icon-mobile"></i>
+                  <i class="icon tim-icons icon-wifi"></i>
+                  <!-- Left 1 -->
+                  <i class="icon icon-sm tim-icons icon-key-25"></i>
+                  <i class="icon icon-sm tim-icons icon-atom"></i>
+                  <i class="icon icon-sm tim-icons icon-satisfied"></i>
+                  <!-- Left 2 -->
+                  <i class="icon tim-icons icon-gift-2"></i>
+                  <i class="icon tim-icons icon-tap-02"></i>
+                  <i class="icon tim-icons icon-wallet-43"></i>
+                </div>
+                <span class="blur-hidden h5 text-primary">Explore InfaCoin</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <h1 class="title">InfaCoin</h1>
+          </div>
+          <div class="col-md-3">
+            <ul class="nav">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#purpose" class="nav-link">
+                  Purpose
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#explore" class="nav-link">
+                  Explore
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            
+          </div>
+          <div class="col-md-3">
+            <h3 class="title">Follow us:</h3>
+            <div class="btn-wrapper profile">
+              <a target="_blank" href="https://twitter.com/halo_infranesia" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a target="_blank" href="https://www.facebook.com/infranesia_official" class="btn btn-icon btn-neutral btn-round btn-simple" data-toggle="tooltip" data-original-title="Like us">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+              <a target="_blank" href="https://infranesia.com" class="btn btn-icon btn-neutral  btn-round btn-simple" data-toggle="tooltip" data-original-title="Follow us">
+                <i class="fab fa-chrome"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="./assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="./assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!-- Chart JS -->
+  <script src="./assets/js/plugins/chartjs.min.js"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="./assets/js/plugins/moment.min.js"></script>
+  <script src="./assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!-- Black Dashboard INFA methods, don't include it in your project! -->
+  <script src="./assets/infa/infa.js"></script>
+  <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      blackKit.initDatePicker();
+      blackKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
+</body>
+
+</html>
